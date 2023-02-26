84256526faac863b7da0a183ce38b6e6b83ff505
vbyurg <v.byurg@gmail.com>
2023-02-26 19:09:20 +0400

    feat: add script for player

--- momentum/script/player.js
+++ momentum/script/player.js
@@ -1,7 +1,109 @@
-const play = document.querySelector(".play");
-const nextPlay = document.querySelector(".play-next");
-const prevPlay = document.querySelector(".play-prev");
+import trackList from './trackList.js';
 
-// console.log(play);
-// console.log(nextPlay);
-// console.log(prevPlay);
+const play = document.querySelector('.play');
+const prevPlay = document.querySelector('.play-prev');
+const playList = document.querySelector('.play-list');
+const nextPlay = document.querySelector('.play-next');
+const isPlay = play.classList.contains('pause');
+let currentlyPlaying = 0;
+const audio = new Audio();
+
+// Play / Pause
+play.addEventListener('click', () => {
+    const isPlay = play.classList.contains('pause');
+
+    // Play
+    function playSong() {
+        audio.src = trackList[currentlyPlaying].src;
+        audio.play();
+        play.classList.add('pause');
+
+    }
+    // Pause
+    function pauseSong() {
+        audio.pause();
+        play.classList.remove('pause');
+    }
+
+    // Check if song is playing
+    if (isPlay) {
+        pauseSong();
+
+    } else {
+        playSong();
+    }
+});
+
+// Play Next Song
+function playNext() {
+    if (currentlyPlaying >= trackList.length - 1) {
+        currentlyPlaying = 0;
+    } else {
+        currentlyPlaying++;
+    }
+    audio.src = trackList[currentlyPlaying].src;
+    audio.currentTime = 0;
+    audio.play();
+    play.classList.add('pause');
+    // isPlay = true;
+}
+
+// Play Previous Song
+function playPrev() {
+    if (currentlyPlaying <= 0) {
+        currentlyPlaying = trackList.length - 1;
+    } else {
+        currentlyPlaying--;
+    }
+    audio.src = trackList[currentlyPlaying].src;
+    audio.currentTime = 0;
+    audio.play();
+    play.classList.add('pause');
+    // isPlay = true;
+}
+
+//  Playback in a circle
+function circlePlay() {
+    if (audio.ended) {
+        // isPlay = false;
+        currentlyPlaying++
+        playNext();
+    }
+}
+
+// Add playlist to DOM
+trackList.forEach((track, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${track.title} / ${track.duration}`;
+    li.dataset.index = index
+    li.classList.add('play-item');
+    playList.appendChild(li);
+});
+
+
+// Play songs from playlist
+const tracksList = document.querySelectorAll('.play-list li');
+
+tracksList.forEach(track => {
+    track.addEventListener('click', function () {
+        const currentIndex = this.dataset.index;
+        // Playing the current song
+        if (isPlay) {
+            audio.pause();
+            play.classList.remove('pause');
+
+        } else {
+            audio.src = trackList[currentIndex].src;
+            audio.currentTime = 0;
+            audio.play();
+            play.classList.add('pause');
+            
+            tracksList.forEach(track => track.classList.remove('item-active'));
+            this.classList.add('item-active');
+        }
+    });
+});
+
+nextPlay.addEventListener('click', playNext);
+prevPlay.addEventListener('click', playPrev);
+audio.addEventListener('ended', circlePlay);
---
+++ momentum/script/trackList.js
@@ -0,0 +1,57 @@
+const trackList = [{
+        title: 'Under Mi Sensi (Particle Remix)',
+        src: '../assets/sounds/Barrington_Levy_-_Under_Mi_Sensi_Particle_Remix_66130638.mp3',
+        duration: '04:33'
+    },
+    {
+        title: 'First Date',
+        src: '../assets/sounds/Blink_182_-_First_Date_48018551.mp3',
+        duration: '02:51'
+    },
+    {
+        title: 'Everything is Magic',
+        src: '../assets/sounds/Angels_Airwaves_-_Everythings_Magic_51372433.mp3',
+        duration: '03:51'
+    },
+    {
+        title: 'Barbie Girl',
+        src: '../assets/sounds/Aqua_-_Barbie_Girl_48004593.mp3',
+        duration: '03:15'
+    },
+    {
+        title: 'Shadow',
+        src: '../assets/sounds/Chromatics_-_Shadow_b128f0d224.mp3',
+        duration: '03:44'
+    },
+    {
+        title: 'Hits from the bong',
+        src: '../assets/sounds/Cypress_Hill_-_Hits_from_the_Bong_48073969.mp3',
+        duration: '02:41'
+    },
+    {
+        title: 'Shut Em Down',
+        src: '../assets/sounds/DMX_Onyx_-_Shut_Em_Down_48276723.mp3',
+        duration: '03:58'
+    },
+    {
+        title: 'Dont Stop',
+        src: '../assets/sounds/Gigamesh_-_dont_stop_69420328.mp3',
+        duration: '03:18'
+    },
+    {
+        title: 'Painkiller',
+        src: '../assets/sounds/Judas_Priest_-_Painkiller_47956347.mp3',
+        duration: '06:04'
+    },
+    {
+        title: 'Creeping death',
+        src: '../assets/sounds/Metallica_-_Creeping_Death_47955425.mp3',
+        duration: '06:36'
+    },
+    {
+        title: 'Creeping',
+        src: '../assets/sounds/Creeping.mp3',
+        duration: '00:41'
+    },
+]
+export default trackList;
