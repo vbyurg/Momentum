72c7ed8fb01ae631198493b15478c456b78b8632
vbyurg <v.byurg@gmail.com>
2023-02-26 20:30:41 +0400

    refactor: reduced the size of songs

--- momentum/script/trackList.js
+++ momentum/script/trackList.js
@@ -1,52 +1,53 @@
 const trackList = [{
         title: 'Under Mi Sensi (Particle Remix)',
         src: '../assets/sounds/Barrington_Levy_-_Under_Mi_Sensi_Particle_Remix_66130638.mp3',
-        duration: '04:33'
+        duration: '02:17'
     },
     {
         title: 'First Date',
         src: '../assets/sounds/Blink_182_-_First_Date_48018551.mp3',
-        duration: '02:51'
+        duration: '00:52'
+
     },
     {
         title: 'Everything is Magic',
         src: '../assets/sounds/Angels_Airwaves_-_Everythings_Magic_51372433.mp3',
-        duration: '03:51'
+        duration: '00:44'
     },
     {
         title: 'Barbie Girl',
         src: '../assets/sounds/Aqua_-_Barbie_Girl_48004593.mp3',
-        duration: '03:15'
+        duration: '00:45'
     },
     {
         title: 'Shadow',
         src: '../assets/sounds/Chromatics_-_Shadow_b128f0d224.mp3',
-        duration: '03:44'
+        duration: '01:53'
     },
     {
         title: 'Hits from the bong',
         src: '../assets/sounds/Cypress_Hill_-_Hits_from_the_Bong_48073969.mp3',
-        duration: '02:41'
+        duration: '01:08'
     },
     {
         title: 'Shut Em Down',
         src: '../assets/sounds/DMX_Onyx_-_Shut_Em_Down_48276723.mp3',
-        duration: '03:58'
+        duration: '00:22'
     },
     {
         title: 'Dont Stop',
         src: '../assets/sounds/Gigamesh_-_dont_stop_69420328.mp3',
-        duration: '03:18'
+        duration: '01:22'
     },
     {
         title: 'Painkiller',
         src: '../assets/sounds/Judas_Priest_-_Painkiller_47956347.mp3',
-        duration: '06:04'
+        duration: '01:20'
     },
     {
         title: 'Creeping death',
         src: '../assets/sounds/Metallica_-_Creeping_Death_47955425.mp3',
-        duration: '06:36'
+        duration: '01:23'
     },
     {
         title: 'Creeping',
